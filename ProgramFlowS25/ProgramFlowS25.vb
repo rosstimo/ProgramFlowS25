388c35d4f73a78ec7556147ce85120cd92115488
tim <rosstimo@isu.edu>
2025-01-31 08:53:45 -0700

    add second number loop and logic

--- ProgramFlowS25/ProgramFlowS25.vb
+++ ProgramFlowS25/ProgramFlowS25.vb
@@ -8,26 +8,62 @@ Module ProgramFlowS25
 
     Sub Main()
         Dim firstNumber As Integer
+        Dim secondNumber As Integer
         Dim getOutOfHere As Boolean = False
         Dim userInput As String
 
         Do
-            Console.WriteLine("Enter a number")
-            Console.WriteLine("Enter Q to quit")
-            userInput = Console.ReadLine()
-            Console.WriteLine($"you entered {userInput}")
-            Try
-                firstNumber = CInt(userInput)
+            'get first number from user
+            'always runs at least once
+            'run until valid number or user quits
 
-            Catch ex As Exception
-                If userInput <> "Q" Then
-                    Console.WriteLine($"{userInput} is not whole number... :(  ")
-                End If
-            End Try
+            Do
+                Console.WriteLine("Enter the first number")
+                Console.WriteLine("Enter Q to quit")
+                userInput = Console.ReadLine()
+                Console.WriteLine($"you entered {userInput}")
+                Try
+                    firstNumber = CInt(userInput)
+                    getOutOfHere = True
+
+                Catch ex As Exception
+                    If userInput <> "Q" Then
+                        Console.WriteLine($"{userInput} is not whole number... :(  ")
+                    Else
+                        getOutOfHere = True
+                    End If
+                End Try
+            Loop Until getOutOfHere = True
+
+            'get second number from user
+            'only runs if user has not already quit
+            'run until valid number or user quits
+            getOutOfHere = False 'reset flag
+            Do While userInput <> "Q" And getOutOfHere = False
+                Console.WriteLine("Enter the second number")
+                Console.WriteLine("Enter Q to quit")
+                userInput = Console.ReadLine()
+                Console.WriteLine($"you entered {userInput}")
+                Try
+                    secondNumber = CInt(userInput)
+                    getOutOfHere = True
+
+                Catch ex As Exception
+                    If userInput <> "Q" Then
+                        Console.WriteLine($"{userInput} is not whole number... :(  ")
+                    Else
+                        getOutOfHere = True
+                    End If
+                End Try
+            Loop
+
+
+            'get user choice 
+            'only runs if user has not already quit
+            'run until valid choice or user quits
 
 
 
-            ' Console.Clear()
 
         Loop While userInput <> "Q"
 
